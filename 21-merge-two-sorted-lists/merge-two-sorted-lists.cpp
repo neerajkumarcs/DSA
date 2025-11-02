0189165ba6d52619cf8c2f75b5939ea5ace9e29d
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2025-11-02 19:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (98.96%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -2,31 +2,59 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // ListNode* tempA=list1;
+        // ListNode* tempB=list2;
+        // ListNode* C=new ListNode(100);
+        // ListNode* tempC=C;
+        // while(tempA!=NULL && tempB!=NULL){
+        //     if(tempA->val<=tempB->val){
+        //         ListNode *t=new ListNode(tempA->val);
+        //         tempC->next=t;
+        //         tempC=t;
+        //         tempA=tempA->next;
+        //     }
+        //     else{
+        //         ListNode* t=new ListNode(tempB->val);
+        //         tempC->next=t;
+        //         tempC=t;
+        //         tempB=tempB->next;
+        //     }
+            
+        // }
+        // if(tempA==NULL){
+        //     tempC->next=tempB;
+        // }
+        // else{
+        //     tempC->next=tempA;
+        // }
+        // return C->next; // space  complexity is O(m+n)
+
+
+
+
+        // now trying do this question in o(n) space complexity 
         ListNode* tempA=list1;
         ListNode* tempB=list2;
-        ListNode* C=new ListNode(100);
-        ListNode* tempC=C;
+        ListNode* list3=new ListNode(100);
+        ListNode* tempC=list3;
         while(tempA!=NULL && tempB!=NULL){
             if(tempA->val<=tempB->val){
-                ListNode *t=new ListNode(tempA->val);
-                tempC->next=t;
-                tempC=t;
+                tempC->next=tempA;
+                tempC=tempA;
                 tempA=tempA->next;
             }
             else{
-                ListNode* t=new ListNode(tempB->val);
-                tempC->next=t;
-                tempC=t;
+                tempC->next=tempB;
+                tempC=tempB;
                 tempB=tempB->next;
             }
-            
         }
-        if(tempA==NULL){
-            tempC->next=tempB;
-        }
-        else{
-            tempC->next=tempA;
-        }
-        return C->next;
+            if(tempA==NULL){
+                tempC->next=tempB;
+            }
+            else{
+                tempC->next=tempA;
+            }
+        return list3->next;
     }
 };
