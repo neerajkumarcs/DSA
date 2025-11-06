8af4db449d95677a8172783dcf9f7cb5a51108e3
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2025-11-06 16:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (9.30%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -2,13 +2,22 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
       // iterated method without use of recurssion
-      ListNode *pre=NULL, *curr=head,*nxt=head;
-      while(curr){
-        nxt=curr->next;
-        curr->next=pre;
-        pre=curr;
-        curr=nxt;
-      }  
-      return pre;
+    //   ListNode *pre=NULL, *curr=head,*nxt=head;
+    //   while(curr){
+    //     nxt=curr->next;
+    //     curr->next=pre;
+    //     pre=curr;
+    //     curr=nxt;
+    //   }  
+    //   return pre;
+
+
+
+    // now do reverse through recurssion 
+    if(head==NULL || head->next==NULL) return head;
+    ListNode* newhead=reverseList(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return newhead;
     }
 };
