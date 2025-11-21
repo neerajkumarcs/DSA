5179324189e87e3ed5fc96f983156999f56528be
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2025-11-22 05:20:36 +0530

    Time: 7 ms (7.59%) | Memory: 77.1 MB (44.86%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -6,17 +6,17 @@ public:
         int carry=0;
         while(l1 || l2 || carry){
             int a=0, b=0;
-            if(l1!=NULL){
+            if(l1!=NULL){ // check kro list one null toh nhi hua
              a=l1->val;
              l1=l1->next;
             }
-            if(l2!=NULL){
+            if(l2!=NULL){ // check kro list 2 toh null nhi hua 
              b=l2->val;
              l2=l2->next;
             }
-            int n=a+b+carry;
-            carry=n/10;
-            int digit=n%10;
+            int n=a+b+carry; // dono list ke values with previous carry ka sum
+            carry=n/10;  // caryy for next round
+            int digit=n%10; // total sum with removal of first digit 
             temp->next=new ListNode(digit);
             temp=temp->next;
             
@@ -24,3 +24,4 @@ public:
         return sum->next;
     }
 };
+// Note* : this code may fail if total sum of two nodes is in three digit 
