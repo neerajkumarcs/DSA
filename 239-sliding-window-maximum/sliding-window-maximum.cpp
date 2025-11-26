e6dce94092dc40b14466ed729547ad5ca29de3b7
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2025-11-26 11:25:33 +0530

    Time: 21 ms (66.02%) | Memory: 142.7 MB (22.79%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        int n = arr.size();                 
+        if (n == 0 || k == 0) return {};
+        if (k == 1) return arr;
+        if (k > n) k = n;
+
+        vector<int> ngi(n);                
+        stack<int> st;
+
+        // step1-> first to find next greater index
+        ngi[n-1] = n;
+        st.push(n-1);
+        for (int i = n-2; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] <= arr[i]) { // correct pop
+                st.pop();
+            }
+            if (st.empty()) ngi[i] = n;
+            else ngi[i] = st.top();
+            st.push(i);
+        }
+
+        // step2-> now traveling in window and find next greatest element
+        vector<int> ans;
+        int j = 0;
+        for (int i = 0; i < n - k + 1; i++) {
+            if (j < i) j = i;
+            int mx = arr[j];
+            while (j < i + k) {
+                mx = arr[j];
+                if (ngi[j] >= i + k) break;
+                j = ngi[j];
+            }
+            ans.push_back(mx);
+        }
+        return ans;
+    }
+};
