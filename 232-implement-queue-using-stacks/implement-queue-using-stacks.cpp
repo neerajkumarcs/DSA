25b0c9c586d49494c1867108c8b32786b897dd09
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-01-06 06:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (28.37%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,3 +1,52 @@
+// // push efficiant approach
+// class MyQueue {
+// public:
+// stack<int> st;
+// stack<int> helper;
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int x) { TC-> O(1)
+//         st.push(x);
+//     }
+    
+//     int pop() { // -> O(n)
+//         while(st.size()>0){
+//          helper.push(st.top());
+//          st.pop();
+//         }
+//         int x=helper.top();
+//         helper.pop();
+//         while(helper.size()>0){
+//            st.push(helper.top());
+//            helper.pop();
+//         }
+//        return x; 
+//     }
+    
+//     int peek() { // front -> O(n)
+//     // retreive from bottom
+//         while(st.size()>0){
+//          helper.push(st.top());
+//          st.pop();
+//         }
+//         int x=helper.top();
+//         while(helper.size()>0){
+//            st.push(helper.top());
+//            helper.pop();
+//         }
+//        return x; 
+//     }
+    
+//     bool empty() {
+//         if(st.size()==0) return true;
+//         else return false;
+//     }
+// };
+
+// POP efficiant approach
+// bs push o(n) mei hoga 
 class MyQueue {
 public:
 stack<int> st;
@@ -6,35 +55,34 @@ stack<int> helper;
         
     }
     
-    void push(int x) {
+    void push(int x) { // TC-O(N)
+    // push at bottom 
+        if(st.size()==0){
+            st.push(x);
+            return;
+        }
+       else{
+        while(st.size()>0){
+            helper.push(st.top());
+            st.pop();
+        }
         st.push(x);
+        while(helper.size()>0){
+            st.push(helper.top());
+            helper.pop();
+        }
+       }
     }
     
-    int pop() {
-        while(st.size()>0){
-         helper.push(st.top());
-         st.pop();
-        }
-        int x=helper.top();
-        helper.pop();
-        while(helper.size()>0){
-           st.push(helper.top());
-           helper.pop();
-        }
+    int pop() { // -> O(1)
+        int x=st.top();
+        st.pop();
        return x; 
     }
     
-    int peek() { // front 
-        while(st.size()>0){
-         helper.push(st.top());
-         st.pop();
-        }
-        int x=helper.top();
-        while(helper.size()>0){
-           st.push(helper.top());
-           helper.pop();
-        }
-       return x; 
+    int peek() { // front -> O(1)
+    // retreive from bottom
+        return st.top();
     }
     
     bool empty() {
