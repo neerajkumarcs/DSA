f8687cf92bc643c1b803dd81b8b9379398ef2d05
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-01-09 05:28:31 +0530

    Time: 20 ms (70.01%) | Memory: 139.3 MB (24.81%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,37 +1,52 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        int n = arr.size();            
-        if (k == 1) return arr;
-        if (k > n) k = n;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // int n = arr.size();            
+        // if (k == 1) return arr;
+        // if (k > n) k = n;
 
-        vector<int> ngi(n);     
-        stack<int> st;
+        // vector<int> ngi(n);     
+        // stack<int> st;
 
-        // step1-> first to find next greater index
-        ngi[n-1] = n;
-        st.push(n-1);
-        for (int i = n-2; i >= 0; i--) {
-            while (st.size()>0 && arr[st.top()] <= arr[i]) { 
-                st.pop();
-            }
-            if (st.size()==0) ngi[i] = n;
-            else ngi[i] = st.top();
-            st.push(i);
-        }
+        // // step1-> first to find next greater index
+        // ngi[n-1] = n;
+        // st.push(n-1);
+        // for (int i = n-2; i >= 0; i--) {
+        //     while (st.size()>0 && arr[st.top()] <= arr[i]) { 
+        //         st.pop();
+        //     }
+        //     if (st.size()==0) ngi[i] = n;
+        //     else ngi[i] = st.top();
+        //     st.push(i);
+        // }
 
-        // step2-> now traveling in window and find next greatest element
+        // // step2-> now traveling in window and find next greatest element
+        // vector<int> ans;
+        // int j = 0;
+        // for (int i = 0; i < n - k + 1; i++) {
+        //     if (j < i) j = i;
+        //     int mx = arr[j];
+        //     while (j < i + k) {
+        //         mx = arr[j];
+        //         if (ngi[j] >= i + k) break;
+        //         j = ngi[j];
+        //     }
+        //     ans.push_back(mx);
+        // }
+        // return ans;
+
+
+
+        // solving this question using sliding window maximum
+        if(k==1) return nums;
+        int n=nums.size();
+        deque<int> dq;
         vector<int> ans;
-        int j = 0;
-        for (int i = 0; i < n - k + 1; i++) {
-            if (j < i) j = i;
-            int mx = arr[j];
-            while (j < i + k) {
-                mx = arr[j];
-                if (ngi[j] >= i + k) break;
-                j = ngi[j];
-            }
-            ans.push_back(mx);
+        for(int i=0 ; i<n; i++){
+            while(dq.size()>0 && nums[dq.back()]<nums[i]) dq.pop_back();
+            dq.push_back(i);
+            while(dq.front()<i-k+1) dq.pop_front();
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
         return ans;
     }
