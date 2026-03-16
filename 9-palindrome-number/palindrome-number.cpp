17a6218c2756395b7753e6f76d7dc8fd467155ae
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-03-16 10:01:00 +0530

    Time: 4 ms (31.55%) | Memory: 8.5 MB (65.17%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false; // negative number never  because a number only one sign allowed 
+        // if(x<0) return false; // negative number never  because a number only one sign allowed 
+        // int orgi=x;
+        // long long rev=0,rem;
+        // while(x!=0){
+        //     rem=x%10;
+        //     rev=rev*10+rem;
+        //     x=x/10;
+        // }
+        // if(rev==orgi) return true;
+        // return false;
+
+        // using reverse half method 
         int orgi=x;
-        long long rev=0,rem;
-        while(x!=0){
+        if(x<0||(x%10==0 && x!=0)) return false;
+        int rev=0, rem;
+        while(x>rev){
             rem=x%10;
             rev=rev*10+rem;
             x=x/10;
         }
-        if(rev==orgi) return true;
+        if(rev==x || rev/10==x) return true; // rev/10==x use to handle the odd cases and rev==x for even one 
         return false;
     }
 };
