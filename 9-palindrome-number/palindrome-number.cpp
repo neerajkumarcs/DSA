fe32aab147e44653902184edc026e8e643fc45fe
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-03-16 09:37:40 +0530

    Time: 6 ms (16.68%) | Memory: 8.6 MB (37.38%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int orgi=x;
+        long long rev=0,rem;
+        while(x>0){
+            rem=x%10;
+            if(rem<0) return false;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(rev==orgi) return true;
+        return false;
+    }
+};
