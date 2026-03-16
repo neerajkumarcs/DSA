ae5c50dacb62195f2253e9135b4b178ee4d93efd
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-03-16 09:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        if(x<0) return false; // negative number never  because a number only one sign allowed 
         int orgi=x;
         long long rev=0,rem;
         while(x!=0){
             rem=x%10;
-            if(rem<0) return false;
             rev=rev*10+rem;
             x=x/10;
         }
