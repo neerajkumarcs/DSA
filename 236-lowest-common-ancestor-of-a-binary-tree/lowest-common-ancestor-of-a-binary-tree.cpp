a64def0e31de659b6106709095590f4859bee40b
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-03-19 02:27:36 +0530

    Time: 555 ms (5.00%) | Memory: 17.4 MB (65.11%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -3,11 +3,16 @@ public:
     bool exist(TreeNode* root, TreeNode* target){
         if(root==NULL) return false;
         if(root==target) return true;
-        return exist(root->left, target) || exist(root->right, target); // target root ke left ya fir right kahi na kahi to pakka hoga 
+        return exist(root->left, target) || exist(root->right, target);
     }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(exist(root->left,p) && exist(root->left,q))  return lowestCommonAncestor(root->left, p,q); // jab dono left mei ho
-         if(exist(root->right,p) && exist(root->right,q)) return lowestCommonAncestor(root->right, p,q); // jab dono targets root ke right mei ho
-         return root; 
+        if(exist(root->left,p) && exist(root->left,q))
+            return lowestCommonAncestor(root->left, p, q);
+
+        if(exist(root->right,p) && exist(root->right,q))
+            return lowestCommonAncestor(root->right, p, q);
+
+        return root;
     }
 };
