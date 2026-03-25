ae083f7c1e5c87eecf5260e1115d64829dcacdb3
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-03-25 12:36:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (49.18%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,43 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        if(root == NULL) return result;
-
+        if(root==NULL ) return result;
         stack<TreeNode*> currlvl;
         stack<TreeNode*> nxtlvl;
-
-        bool leftToRight = true;
+        vector<int> lvl;// use to store every level
         currlvl.push(root);
-
-        vector<int> level;
-
+        bool lefttoright=true;
         while(!currlvl.empty()){
-            TreeNode* temp = currlvl.top();
+            TreeNode* temp=currlvl.top();
+            lvl.push_back(temp->val);
             currlvl.pop();
-
             if(temp){
-                level.push_back(temp->val);
-
-                if(leftToRight){
+                if(lefttoright){
                     if(temp->left) nxtlvl.push(temp->left);
                     if(temp->right) nxtlvl.push(temp->right);
-                } else {
+                }
+                else{
                     if(temp->right) nxtlvl.push(temp->right);
                     if(temp->left) nxtlvl.push(temp->left);
                 }
             }
-
-            // end of level
-            if(currlvl.empty()){
-                result.push_back(level);   
-                level.clear();             // to become again emplty
-
-                leftToRight = !leftToRight; // flip direction
-                swap(currlvl, nxtlvl);
-            }
+                // after level
+                if(currlvl.empty()){
+                    result.push_back(lvl);
+                    lvl.clear(); // for resting the 1d vector
+                    lefttoright=!lefttoright;
+                    swap(currlvl,nxtlvl);
+            
+                }
         }
-
         return result;
     }
 };
