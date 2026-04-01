8ea50f70c53b3529cca852e9d01f67db04652ee6
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-04-02 01:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (94.84%) - LeetSync

---
+++ 112-path-sum/path-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+bool helper(TreeNode* root, int sum){
+    if(root==NULL) return  false;
+    if(root->left==nullptr && root->right==nullptr){
+        if(root->val==sum) return true;
+    }
+    return helper(root->left, sum-root->val)|| helper(root->right, sum-root->val);
+    
+
+}
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL) return false;
+        return helper(root, targetSum);
+    }
+};
