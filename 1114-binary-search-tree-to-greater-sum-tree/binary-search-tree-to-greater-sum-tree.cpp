48738ebeaf41fd4eb4636497a785c1b2ea40d2a9
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-04-07 01:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (48.77%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    void inorder(TreeNode* root, int &sum){
+    void inorder(TreeNode* root, int &sum){ // inorder traversal from right to left (means reverse order)
         if(root==nullptr) return ;
         inorder(root->right,sum);
-        sum+=root->val;
-        root->val=sum;
+        sum+=root->val; // previous root ka values ko store kr rha hai 
+        root->val=sum; // root ki value ka modification ho rha hai 
         inorder(root->left,sum);
     }
     TreeNode* bstToGst(TreeNode* root) {
