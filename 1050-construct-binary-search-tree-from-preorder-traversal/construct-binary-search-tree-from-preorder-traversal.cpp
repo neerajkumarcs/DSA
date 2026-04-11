80d8ba0d057c2e74daa56b516b64d7bd4ee67d13
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-04-11 16:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (99.47%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,23 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* build(vector<int>&pre, int prelow, int prehigh, vector<int> & in, int inlow, int inhigh){
-        if(prelow>prehigh) return NULL;
-        TreeNode* root=new TreeNode(pre[prelow]);
-        int i=inlow;
-        while(i<=inhigh){
-            if(in[i]==pre[prelow]) break;
-            i++;
+    void insert(TreeNode* root, int val){
+        if(root==NULL) root=new TreeNode(val);
+        else if(root->val>val){ // go to left
+        if(root->left==NULL) root->left=new TreeNode(val);
+        else insert(root->left, val); // go to left until null occure or move to right side 
         }
-        int leftcount=i-inlow;
-        int rightcount=inhigh-i;
-        root->left= build(pre,prelow+1,prelow+leftcount, in,inlow,i-1);
-        root->right= build(pre,prelow+leftcount+1,prehigh, in, i+1, inhigh);
-        return root;
+        else{ // root->val<val
+        // attach to right
+        if(root->right==nullptr) root->right=new TreeNode(val);
+        else insert(root->right, val);// go to right until null occure or move to left side 
+        }
     }
-    TreeNode* bstFromPreorder(vector<int>& pre) {
-        vector<int> in= pre;
-        sort(in.begin(), in.end());
-        int n=in.size();
-        return build(pre,0,n-1,in, 0,n-1);
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+      TreeNode* root=new TreeNode(preorder[0]);
+      for(int i=1; i<preorder.size(); i++ ){
+        insert(root, preorder[i]);
+      } 
+      return root; 
     }
 };
