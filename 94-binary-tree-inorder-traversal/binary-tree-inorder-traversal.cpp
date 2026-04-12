71cfafd080cca208ce074fd36c2d02be0843f6cb
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-04-12 18:12:48 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (14.04%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+// inorder morris traversal 
     vector<int> inorderTraversal(TreeNode* root) {
+    if(root==NULL) return {};
     vector<int> ans;
     TreeNode* curr=root;
     while(curr){
-        if(curr->left){
+        if(curr->left){ // to find predecessor 
             TreeNode* pre=curr->left;
             while(pre->right!=NULL && pre->right!=curr){
                 pre=pre->right;
             }
-        if(pre->right==NULL){ // when pre-> right reach at dead end 
+        if(pre->right==NULL){ // when pre-> right reach at dead end (for linking )
             pre->right=curr;
             curr=curr->left;
         }    
-        else{ // when pre->right== curr
+        else{ // when pre->right== curr(for unlinking)
             pre->right=nullptr;
             ans.push_back(curr->val);
             curr=curr->right;
         }
         }
-    else {
+    else { // last case when current left==NULL
         ans.push_back(curr->val);
         curr=curr->right;
         }  
