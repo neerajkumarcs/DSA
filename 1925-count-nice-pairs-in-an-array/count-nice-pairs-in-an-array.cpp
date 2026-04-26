1c9694bba72c2e1c7a16f76acca6d698f4c0d00a
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-04-27 03:18:59 +0530

    Time: 19 ms (77.97%) | Memory: 60.9 MB (62.46%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
@@ -16,11 +16,11 @@ public:
         for(int i=0; i<n; i++){
             int val=(nums[i]-rev(nums[i]));
            if(m.find(val)!=m.end()) {
-            count=count%1000000007;
+            //count=count%1000000007;
             count+=m[val];
            }
            m[val]++;
         }
-    return count%1000000007;
+    return count%1000000007; // Return the number of nice pairs of indices. Since that number can be too large, return it modulo 109 + 7. 
     }
 };
