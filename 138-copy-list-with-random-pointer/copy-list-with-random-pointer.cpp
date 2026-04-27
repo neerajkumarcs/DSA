f1f47579f8edf1103c7f5575868041b82c6bbdf8
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-04-28 01:28:18 +0530

    Time: 3 ms (91.81%) | Memory: 15.3 MB (26.94%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,66 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head) return nullptr; // added safety
-
-        // step 1: create the deep copy withoud random pointer
-        Node* temp = head;
-        Node *c = new Node(100);
-        Node* tempc = c;
-        while (temp) {
-            Node* a = new Node(temp->val);
-            tempc->next = a;
-            temp = temp->next;
-            tempc = tempc->next;
+        if(!head) return NULL;
+        // step-> create a new list without random pointer
+        Node* dummy=new Node(0);
+        Node* tempC=dummy;
+        Node* temp=head;
+        while(temp){
+            Node* a=new Node(temp->val);
+            tempC->next=a;
+            temp=temp->next;
+            tempC=a;
         }
-        Node* dub = c->next;
-        // step 2: merging alternage connections  both old and new lists 
-        Node* a = head;
-        Node* b = dub;
-        Node* merge = new Node(100);
-        Node* tempd = merge;
-        while (a != nullptr && b != nullptr) {
-            Node* nextA = a->next;
-            Node* nextB = b->next;
-
-            tempd->next = a;
-            tempd = tempd->next;
-
-            tempd->next = b;
-            tempd = tempd->next;
-
-            a = nextA;
-            b = nextB;
+        Node* b=dummy->next;
+        Node* a=head;
+        Node* tempa=a;
+        Node* tempb=b;
+        // step 2-> To make a map of <originalnode,dummynode>
+        unordered_map<Node*, Node*>m;
+        while(tempa){
+            m[tempa]=tempb;
+            tempa=tempa->next;
+            tempb=tempb->next;
         }
-        if (a) tempd->next = a;
-        if (b) tempd->next = b;
-        merge = merge->next;
-        // step 3: assinging random pointers to dublicate 
-        Node* t1 = merge;
-        while (t1 && t1->next) {
-            Node* t2 = t1->next;
-            if (t1->random) t2->random = t1->random->next;
-            // move to next original node
-            t1 = t1->next->next;
+        // step 3-> now connect randrom pointers of dummy node with help of map
+        for(auto x : m){
+            Node* o =x.first;
+            Node* d= x.second;
+           if(o->random){
+            Node* orandeom=o->random;
+            Node* drandom=m[o->random];
+            d->random=drandom;
+           }
         }
-        // step 4 separating  the merged node
-        t1 = merge; // reset t1 to start of merged list
-        Node* newhead = new Node(100);
-        Node* final = newhead;
-        while (t1 && t1->next) {
-            Node* original = t1;
-            Node* copy = t1->next;
-
-            final->next = copy;
-            final = final->next;
-
-            // restore original next
-            original->next = copy->next;
-
-            // advance
-            t1 = original->next;
-        }
-        final->next = nullptr;
-        return newhead->next;
+        return b;
     }
 };
