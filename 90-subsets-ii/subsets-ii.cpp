a0758d6d80d3095751ef2c97755af24ee6754e2a
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-08-17 01:31:08 +0530

    Time: 8 ms (5.94%) | Memory: 17.5 MB (5.09%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> v;
-        int n = nums.size();
-
-        sort(nums.begin(), nums.end());
-
-        // Generate all 2^n subsets
-        for(int k = 0; k < (1 << n); k++) {
-            vector<int> t;
-
-            for(int i = 0; i < n; i++) {
-                if(k & (1 << i)) {
-                    t.push_back(nums[i]);
-                }
-            }
-
-            v.push_back(t);
+    void helper(vector<int>&nums, set<vector<int>>&s, vector<int>v,int idx){
+        if(idx==nums.size()){
+            s.insert(v);
+            return ;
         }
-
-        // Remove duplicate subsets
-        sort(v.begin(), v.end());
-        v.erase(unique(v.begin(), v.end()), v.end());
-
-        return v;
+        helper(nums,s,v, idx+1);
+        v.push_back(nums[idx]);
+        helper(nums, s, v, idx+1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>>s;
+        vector<int> v;
+        helper(nums,s,v,0);
+        return vector<vector<int>>(s.begin(), s.end());
     }
 };
