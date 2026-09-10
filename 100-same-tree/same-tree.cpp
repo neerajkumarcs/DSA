d4b4677b6f20857934805f755d0ed34daba5bdb5
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-09-10 11:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.34%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -4,10 +4,10 @@ public:
      if(p==NULL && q==NULL) return true;
      if(p==NULL || q==NULL) return false;
      if(p->val!=q->val) return false;
-     bool leftans=isSameTree(p->left, q->left);
+     bool leftans=isSameTree(p->left,q->left);
      if(leftans==false) return false;
      bool rightans=isSameTree(p->right, q->right);
-     if(rightans==false ) return false;
-     return true;   
+     if(rightans==false) return false;
+     return true;
     }
 };
