1d27ae29b5b8c3d6a99c3b375bbd9251bd5f1f55
Neeraj Kumar <neerajkumar.cs24@gmail.com>
2026-09-24 17:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (99.82%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,4 +1,13 @@
-  
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
@@ -6,20 +15,26 @@ public:
         int len=0;
         ListNode* temp=head;
         ListNode* tail;
-        while(temp!=NULL){
-            if(temp->next==NULL) tail=temp; // yaha maine tail ko last node pe setup kiya
+        while(temp){
+            if(temp->next==NULL) tail=temp;
             temp=temp->next;
             len++;
         }
-        temp=head; // temp ko dubara initialise kiya
-        k=k%len; // if k is very large used this technique to reduce no. of cycles
+        k=k%len;
         if(k==0) return head;
-        for(int i=1; i<len-k; i++){ // yaha mei temp ko just ek node pahle setup kiya jaha se mere ko rotation strt krni hai
-            temp=temp->next;
+        temp=head;
+        ListNode* b=head;
+        while(k){
+            b=b->next;
+            k--;
         }
-        tail->next=head; // last point to first 
-        head=temp->next; //  first point at kth node from last 
-        temp->next=NULL; // after rotation break the circular link list
-        return head;
+        while(b->next){
+            temp=temp->next;
+            b=b->next;
+        }
+        b->next=head;
+        ListNode* newhead=temp->next;
+        temp->next=NULL;
+        return newhead;
     }
 };
